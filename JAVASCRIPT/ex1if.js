479fe343884afccdd17b76b393bfccd4eeeda746
sarthak-gadge <sathakgadge2002@gmail.com>
2024-10-07 19:25:20 +0530

    add existing files

---
+++ JAVASCRIPT/ex1if.js
@@ -0,0 +1,10 @@
+var age;
+age=prompt("enter a age:");
+if(age>=18)
+{
+document.write("eligible for voting");
+}
+else
+{
+document.write("not eligible for voting");
+}
---
+++ JAVASCRIPT/ex2.js
@@ -0,0 +1,4 @@
+var n,cube;
+n=prompt("enter a number: ");
+cube=n*n*n;
+document.write("cube is: "+cube);
